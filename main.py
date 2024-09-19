f5172aadb3dc3708511563ae8d63a6f11abba02c
Dimarik444 <kosonogov.dmitr@gmail.com>
2024-09-19 18:37:59 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -7,11 +7,7 @@ print (9.99 > 9.98 and 1000 != 1000.1)
 # "3rd programm"
 print (2 * 2 + 2)
 print (2 * (2 + 2))
-print ((2 * 2 + 2) == (2 * (2 + 2)))
+print (2 * 2 + 2 == 2 * (2 + 2))
 
 # "4th programm"
-print("123.456")
-print(str("123.456"))
-print(123.456 * 10)
-print(int(1234.56))
-print(1234 % 10)
+print ((int(float("123.456") * 10)) % 10)
