7af496d8d4b7ff207db21513eb94ff8cf376b449
Dimarik444 <kosonogov.dmitr@gmail.com>
2024-09-24 17:55:57 +0300

    Add files via upload

---
+++ задание2.py
@@ -0,0 +1,9 @@
+kolichestvo_vipolnenih_zadanii = 12
+print(kolichestvo_vipolnenih_zadanii)
+kolichestvo_zatrachenih_chasov = 1.5
+print(kolichestvo_zatrachenih_chasov)
+name_kursa = "Python"
+print(name_kursa)
+Vremya_na_odno_zadanie = kolichestvo_zatrachenih_chasov / kolichestvo_vipolnenih_zadanii
+print(Vremya_na_odno_zadanie)
+print("Курс:", name_kursa, "Всего задач:", kolichestvo_vipolnenih_zadanii, "Затрачено часов:", kolichestvo_zatrachenih_chasov, "Среднее время выполнения:", Vremya_na_odno_zadanie)
