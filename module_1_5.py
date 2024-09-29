9ebc75a3f921644490bc6d2336c9ab4176aa02f1
Dimarik444 <kosonogov.dmitr@gmail.com>
2024-09-29 20:18:10 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,8 @@
+immutable_var_ = 5, 6, 7, 8, True, "python"
+print(immutable_var_)
+mutable_list = [1, 2, 3, 4, "питон"]
+print(mutable_list)
+mutable_list[1] = 7
+print(mutable_list)
+print("питон" in mutable_list)
+print(mutable_list [1::2])
