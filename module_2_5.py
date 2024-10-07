c6d37a382d9d7dcb5c3506cf83fdae8f7fefb57d
Крым наш) <kosonogov.dmitr@gmail.com>
2024-10-07 19:46:10 +0300

    06/10

--- module_2_5.py
+++ module_2_5.py
@@ -1,12 +1,16 @@
-def get_matrix(n, m, value):
+def get_matrix(n,m,value):
     matrix = []
+    if n <= 0 :
+        return (matrix)
+    elif m <= 0:
+        return (matrix)
+    else:
+        print()
     for i in range(n):
-        strings = []
-        matrix.append(strings)
+        matrix.append([])
         for j in range(m):
-            strings.append(value)
-    return matrix
-
+            matrix[i].append(value)
+    return(matrix)
 result1 = get_matrix(2, 2, 10)
 result2 = get_matrix(3, 5, 42)
 result3 = get_matrix(4, 2, 13)
