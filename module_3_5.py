69794a1aadef61ac9b65a0410264a8cd27b374b2
Крым наш) <kosonogov.dmitr@gmail.com>
2024-10-20 20:33:15 +0300

    18/10

--- module_3_5.py
+++ module_3_5.py
@@ -1,11 +1,20 @@
 def get_multiplied_digits(number):
     str_number=str(number)
+    while str_number.endswith("0"):
+        str_number = str_number [:-1]
     first=int(str_number[0])
 
     if len(str_number) > 1:
+    
         return first * get_multiplied_digits(int(str_number[1:]))
-    else:
+    else :
         return first
 
 result=get_multiplied_digits(40203)
 print(result)
+
+
+
+
+
+
